1051a404dce7f0c856e3cc520bf9d6df4b448a21
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-21 17:09:50 +0000

    Create `templates` helper. Move tpl into file.

--- application.rb
+++ application.rb
@@ -1,6 +1,16 @@
 require 'rubygems'
 require 'sinatra'
 
+helpers do
+  def templates
+    Dir.glob("templates/**").map do |template|
+      name = File.basename(template, ".html")
+      content = open(template).read
+      "<textarea id='#{name}' class='x-hidden-display'>#{content}</textarea>"
+    end.join("\n")
+  end
+end
+
 get '/' do
   erb :index
 end
