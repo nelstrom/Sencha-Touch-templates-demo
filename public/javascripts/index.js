3595bd9ff820cb9e0c5f13adac1945f4b6947b4f
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-21 15:37:21 +0000

    Add 'for' loop to tpl; render list of planets.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,12 @@
 Ext.setup({
   onReady: function() {
-    var content, planetInfo, planetEarth, planetMars;
+    var content, planetInfo, planetEarth, planetMars, planets;
 
     planetInfo = new Ext.XTemplate(
+      '<tpl for=".">',
       '<h2>{name}</h2>',
-      'mass: {mass}<br/>',
-      'satelites: {satelites:htmlEncode}',
-      '<p>{abstract:ellipsis(100, true)}</p>'
+      'mass: {mass}',
+      '</tpl>'
     );
 
     content = new Ext.Panel({
@@ -15,20 +15,10 @@ Ext.setup({
       tpl: planetInfo
     });
     
-    planetEarth = {
-      name: "Earth",
-      mass: 1.00,
-      satelites: "<The moon>",
-      abstract: "Earth (or the Earth) is the third planet from the Sun, and the densest and fifth-largest of the eight planets in the Solar System. It is also the largest of the Solar System's four terrestrial planets. It is sometimes referred to as the World, the Blue Planet, or by its Latin name, Terra."
-    };
-    
-    planetMars  = {
-      name: "Mars",
-      mass: 0.11,
-      satelites: "<Phobos> & <Deimos>",
-      abstract: "Mars is the fourth planet from the Sun in the Solar System. The planet is named after the Roman god of war, Mars. It is often described as the 'Red Planet', as the iron oxide prevalent on its surface gives it a reddish appearance. Mars is a terrestrial planet with a thin atmosphere, having surface features reminiscent both of the impact craters of the Moon and the volcanoes, valleys, deserts, and polar ice caps of Earth."
-    };
+    planetEarth = { name: "Earth", mass: 1.00 };
+    planetMars  = { name: "Mars",  mass: 0.11 };
+    planets = [planetEarth, planetMars];
 
-    content.update(planetMars);
+    content.update(planets);
   }
 });
